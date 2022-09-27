519b0549c6e3c097aea9b06c29b51d9cffa9cfb6
Dmitrii Nechaev <v-dnechaev@microsoft.com>
2022-09-27 19:07:18 +0200

    Initial commit

---
+++ PCB_Test.DataProvider.Interfaces/IDataProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PCB_Test.DataProvider.Interfaces
+{
+    public interface IDataProvider
+    {
+    }
+}
---
+++ PCB_Test.Services/Implementations/EstimateService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PCB_Test.Services.Implementations
+{
+    class EstimateService
+    {
+    }
+}
